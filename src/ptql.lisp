9f1f2edced81216f214f41dedd868a3ba315becd
Carlo Sganzerla <carlosganzrl@hotmail.com>
2021-10-07 19:54:12 -0300

    Did the fkn macro dude wtf

--- src/ptql.lisp
+++ src/ptql.lisp
@@ -1,18 +1,28 @@
 (in-package #:ptql)
 
 (defmacro create-row-expr (row expr)
-  (cons (car expr) 
-        (reduce (lambda (expr sym)
-                  (subst `(getf ,row ,sym) (intern (symbol-name sym)) expr))
-                (remove-if-not #'symbolp (coerce-symbol row))
-                :initial-value (cdr expr))))
+  (with-gensyms (m-row m-expr)
+    `(let ((,m-row ,row) (,m-expr ',expr))
+       (cons (car ,m-expr) 
+             (reduce (lambda (r-expr sym)
+                       (subst (getf ,m-row sym) 
+                              (intern (symbol-name sym)) 
+                              r-expr))
+                     (remove-if-not #'symbolp ,m-row)
+                     :initial-value (cdr ,m-expr))))))
+
+(defmacro where (rows expr)
+  `(remove-if-not (lambda (row) (create-row-expr row ,expr)) ,rows))
 
 (defmacro select (syms &key from where)
-  (let ((keys (mapcar (lambda (sym) (intern-keyword sym)) 
+  (let ((keys (mapcar (lambda (sym) (intern-keyword sym))
                       (mapcar #'symbol-name syms))))
     `(mapcar (lambda (row) (get-keys row ',keys))
              (table-rows (symbol-value (intern-global ,(symbol-name from)))))))
 
+
+(let ((s (table-rows *table*)))
+  (where s (eql id 1)))
 
 (create-row-expr *row* (or (+ a c) (> c (- b a))))
 (create-row-expr (list :a 1 :b 4 :c 5) (and (+ (c + (c a) b ) a) nil))
